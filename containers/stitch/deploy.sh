817e3e48b87417bf061bdb5008bf5ac556d6fed7
Mike DeLaurentis <miked@stitchdata.com>
2017-01-30 15:33:09 -0500

    Change env vars and repo name

--- containers/stitch/deploy.sh
+++ containers/stitch/deploy.sh
@@ -7,7 +7,7 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 
-name=stitchdata/stream-hubspot
+name=platform.hubspot
 version=$1
 repo=218546966473.dkr.ecr.us-east-1.amazonaws.com
 docker build -t $name:$version -f Dockerfile ../..
