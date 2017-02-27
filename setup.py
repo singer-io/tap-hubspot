f552701e8c595bff84bc36375f5fe43ddaa1dbd4
John Miller <jmiller@rjmetrics.com>
2017-02-27 11:48:49 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-hubspot',
-      version='0.3.2',
+      version='0.3.3',
       description='Singer.io tap for extracting data from the HubSpot API',
       author='Stitch',
       url='http://singer.io',
