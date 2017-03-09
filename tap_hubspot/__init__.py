872a66c9cac2f1cf95445f72166f3bda8dfad190
John Miller <jmiller@rjmetrics.com>
2017-03-09 15:45:15 -0500

    user agent

--- tap_hubspot/__init__.py
+++ tap_hubspot/__init__.py
@@ -155,6 +155,9 @@ def request(url, params=None):
 
     params = params or {}
     headers = {'Authorization': 'Bearer {}'.format(CONFIG['access_token'])}
+    if 'user_agent' in CONFIG:
+        headers['User-Agent'] = CONFIG['user_agent']
+
     req = requests.Request('GET', url, params=params, headers=headers).prepare()
     logger.info("GET {}".format(req.url))
     resp = session.send(req)
