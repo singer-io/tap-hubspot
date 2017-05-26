3a245a5ffa90b3710869e6347e20a9d17e2ca181
Joshua Karstendick <joshua@stitchdata.com>
2017-05-26 15:20:48 -0400

    update unit tests

--- tests/test_tap_hubspot.py
+++ tests/test_tap_hubspot.py
@@ -5,35 +5,35 @@ class TestTapHubspot(unittest.TestCase):
 
     def test_get_streams_to_sync_with_no_this_stream(self):
         streams = [
-            Stream('a', 'a'),
-            Stream('b', 'b')
+            Stream('a', 'a', ['id']),
+            Stream('b', 'b', ['id'])
         ]
         state = {}
         self.assertEqual(streams, get_streams_to_sync(streams, state))
 
     def test_get_streams_to_sync_with_no_this_stream(self):
         streams = [
-            Stream('a', 'a'),
-            Stream('b', 'b'),
-            Stream('c', 'c'),
+            Stream('a', 'a', ['id']),
+            Stream('b', 'b', ['id']),
+            Stream('c', 'c', ['id']),
         ]
         state = {'this_stream': None}
         self.assertEqual(streams, get_streams_to_sync(streams, state))
 
     def test_get_streams_to_sync_with_this_stream(self):
         streams = [
-            Stream('a', 'a'),
-            Stream('b', 'b'),
-            Stream('c', 'c'),
+            Stream('a', 'a', ['id']),
+            Stream('b', 'b', ['id']),
+            Stream('c', 'c', ['id']),
         ]
         state = {'this_stream': 'b'}
         self.assertEqual(streams[1:], list(get_streams_to_sync(streams, state)))
 
     def test_get_streams_to_sync_throws_on_bad_state(self):
         streams = [
-            Stream('a', 'a'),
-            Stream('b', 'b'),
-            Stream('c', 'c'),
+            Stream('a', 'a', ['id']),
+            Stream('b', 'b', ['id']),
+            Stream('c', 'c', ['id']),
         ]
         with self.assertRaises(Exception):
             get_streams_to_sync(streams, {'this_stream': 'Some bad stream'})
