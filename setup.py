3f7fb107a1b1f118edab903c8fb8d73585b5ca3d
Paul Santa Clara <psantaclara@rjmetrics.com>
2017-06-28 15:02:46 -0400

    support for stream aliasing

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(name='tap-hubspot',
       py_modules=['tap_hubspot'],
       install_requires=[
           'attrs==16.3.0',
-          'singer-python==1.6.0',
+          'singer-python==1.9.3',
           'requests==2.12.4',
           'backoff==1.3.2',
           'requests_mock==1.3.0'
