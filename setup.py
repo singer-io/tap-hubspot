e076f2563c0b55b33347adea41db9a02127596f6
Joshua Karstendick <joshdick@gmail.com>
2017-08-18 10:26:46 -0400

    Fix keywords bookmark (#42)
    * fix the copy pasta
    
    * version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-hubspot',
-      version='1.0.4',
+      version='1.0.5',
       description='Singer.io tap for extracting data from the HubSpot API',
       author='Stitch',
       url='http://singer.io',
