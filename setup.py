29ad461d49606e0e7edf1547faa96e4a0d333b7d
Paul Santa Clara <psantaclara@rjmetrics.com>
2017-12-21 16:49:06 -0500

    bumping major version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-hubspot',
-      version='1.0.15',
+      version='2.0.0',
       description='Singer.io tap for extracting data from the HubSpot API',
       author='Stitch',
       url='http://singer.io',
