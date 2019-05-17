c86c2bd81beaf73cfc47f78ae7017dd14af50cb9
Dan Mosora <dan@stitchdata.com>
2019-05-17 19:00:51 +0000

    Add log message about adjusting bookmark

--- tap_hubspot/__init__.py
+++ tap_hubspot/__init__.py
@@ -708,6 +708,8 @@ def sync_engagements(STATE, ctx):
     current_sync_start = utils.now()
     if has_bookmark(STATE, "engagements", bookmark_key) and \
        last_sync_duration is not None:
+        LOGGER.info(("Last sync of engagements lasted {} seconds. Adjusting bookmark by this"
+                     "amount to account for race conditions with record updates."))
         start = start - datetime.timedelta(seconds=last_sync_duration)
     max_bk_value = start
     LOGGER.info("sync_engagements from %s", start)
