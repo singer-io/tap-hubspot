92b964466f91c6fd7dad1c25c7d4c10f6c5de9cc
Andy Lu <andy@stitchdata.com>
2019-07-23 14:51:19 +0000

    Bump to v2.5.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-hubspot',
-      version='2.4.1',
+      version='2.5.0',
       description='Singer.io tap for extracting data from the HubSpot API',
       author='Stitch',
       url='http://singer.io',
