945723ba908a2eb8c7055ac9ee5eb77f36fa24f3
Emilien <esanchez@mediarithmics.com>
2020-07-05 22:06:39 +0200

    Fix issue with typing for postgres and bigquery

--- tap_hubspot/__init__.py
+++ tap_hubspot/__init__.py
@@ -135,7 +135,7 @@ def get_field_type_schema(field_type):
     elif field_type == "number":
         # A value like 'N/A' can be returned for this type,
         # so we have to let this be a string sometimes
-        return {"type": ["null", "number", "string"]}
+        return {"type": ["null", "number"]}
 
     else:
         return {"type": ["null", "string"]}
