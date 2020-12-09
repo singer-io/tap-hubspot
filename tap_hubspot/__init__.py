45b2e312acb990355d51332a17f9e5f36bc0b5db
Patrick-Ranjit D. Madsen <pamad05@gmail.com>
2020-12-09 10:49:36 +0100

    CR: use exception

--- tap_hubspot/__init__.py
+++ tap_hubspot/__init__.py
@@ -8,16 +8,18 @@ from collections import defaultdict
 from typing import DefaultDict, Set
 
 STREAMS = {
+    "companies": {"bookmark_key": "updatedAt"},
     "owners": {"bookmark_key": "updatedAt"},
     "forms": {"bookmark_key": "updatedAt"},
-    "contacts": {"bookmark_key": "updatedAt",},
+    "contacts": {
+        "bookmark_key": "updatedAt",
+    },
     "contacts_events": {"bookmark_key": "lastSynced"},
     "deal_pipelines": {"bookmark_key": "updatedAt"},
     "engagements": {"bookmark_key": "lastUpdated"},
     "submissions": {},
     "email_events": {"bookmark_key": "created"},
     "deals": {"bookmark_key": "updatedAt"},
-    "companies": {"bookmark_key": "updatedAt"},
 }
 REQUIRED_CONFIG_KEYS = [
     "start_date",
@@ -40,8 +42,8 @@ def sync(config, state=None):
             )
             state, event_state = stream.do_sync(state, event_state)
 
-        except Exception as err:
-            LOGGER.error(err, stack_info=True)
+        except Exception:
+            LOGGER.exception(f"{tap_stream_id} failed")
             continue
 
 
