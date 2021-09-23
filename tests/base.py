2dee9eb89a1ecddfdeddee470f256e1c0ca5fd63
Yusaku Kasahara <mark.kasahara@gmail.com>
2021-09-23 07:21:30 -0700

    Qa/start date test (#168)
    * made TODOs for pagination
    
    * updates to pagination test, fix get for contact_lists and create for contacts_by_company
    
    * companies and contacts by company now passing
    
    * subscription_changes is complete
    
    * moved pagination limits to base and put in setUp timer
    
    * added engagements
    
    * [skip ci] removed outdated TODOs
    
    * removed expected_page_size()
    
    * removed comments and cleanup
    
    * fixed deletion mistake
    
    * fixed issues with owners streams and removed deal_pipelines from startdate test
    
    * removed old bookmarks tests
    
    * removed comment
    
    * separated streams with no creates and set hard-coded start dates
    
    * updated comments
    
    * added one more assertion with pks with first and second sync

--- tests/base.py
+++ tests/base.py
@@ -18,7 +18,7 @@ class HubspotBaseTest(unittest.TestCase):
     START_DATE_FORMAT = "%Y-%m-%dT00:00:00Z" # %H:%M:%SZ
     BASIC_DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
     EXPECTED_PAGE_SIZE = "expected-page-size"
-    
+    OBEYS_START_DATE = "obey-start-date"
 
     #######################################
     #  Tap Configurable Metadata Methods  #
@@ -60,71 +60,84 @@ class HubspotBaseTest(unittest.TestCase):
             "campaigns": {
                 self.PRIMARY_KEYS: {"id"},
                 self.REPLICATION_METHOD: self.FULL,
+                self.OBEYS_START_DATE: False 
             },
             "companies": {
                 self.PRIMARY_KEYS: {"companyId"},
                 self.REPLICATION_METHOD: self.INCREMENTAL,
                 self.REPLICATION_KEYS: {"hs_lastmodifieddate"},
                 self.EXPECTED_PAGE_SIZE: 250,
+                self.OBEYS_START_DATE: True
             },
             "contact_lists": {
                 self.PRIMARY_KEYS: {"listId"},
                 self.REPLICATION_METHOD: self.INCREMENTAL,
                 self.REPLICATION_KEYS: {"updatedAt"},
-                self.EXPECTED_PAGE_SIZE: 250
+                self.EXPECTED_PAGE_SIZE: 250,
+                self.OBEYS_START_DATE: True
             },
             "contacts": {
                 self.PRIMARY_KEYS: {"vid"},  # DOCS_BUG listed in stitch docs as 'canonical-vid'
                 self.REPLICATION_METHOD: self.INCREMENTAL,
                 self.REPLICATION_KEYS: {"versionTimestamp"},  # DOCS_BUG  was commented out in OG tests
-                self.EXPECTED_PAGE_SIZE: 100
+                self.EXPECTED_PAGE_SIZE: 100,
+                self.OBEYS_START_DATE: True
             },
             "contacts_by_company": {
                 self.PRIMARY_KEYS: {"company-id", "contact-id"},
                 self.REPLICATION_METHOD: self.FULL,
                 self.EXPECTED_PAGE_SIZE: 100,
+                self.OBEYS_START_DATE: True
             },
             "deal_pipelines": {
                 self.PRIMARY_KEYS: {"pipelineId"},
                 self.REPLICATION_METHOD: self.FULL,
+                self.OBEYS_START_DATE: True
             },
             "deals": {
                 self.PRIMARY_KEYS: {"dealId"},  # DOCS_BUG docs list 'dealId' and 'portalId
                 self.REPLICATION_METHOD: self.INCREMENTAL,
                 self.REPLICATION_KEYS: {"hs_lastmodifieddate"},
+                self.OBEYS_START_DATE: True
             },
             "email_events": {
                 self.PRIMARY_KEYS: {"id"},
                 self.REPLICATION_METHOD: self.INCREMENTAL,
                 self.REPLICATION_KEYS: {"startTimestamp"},  # DOCS_BUG docs list 'id' but OG tests use
-                self.EXPECTED_PAGE_SIZE: 1000
+                self.EXPECTED_PAGE_SIZE: 1000,
+                self.OBEYS_START_DATE: True
             },
             "engagements": {
                 self.PRIMARY_KEYS: {"engagement_id"},  # DOCS_BUG docs list 'id'
                 self.REPLICATION_METHOD: self.INCREMENTAL,
                 self.REPLICATION_KEYS: {"lastUpdated"},
-                self.EXPECTED_PAGE_SIZE: 250
+                self.EXPECTED_PAGE_SIZE: 250,
+                self.OBEYS_START_DATE: True
             },
             "forms": {
                 self.PRIMARY_KEYS: {"guid"},
                 self.REPLICATION_METHOD: self.INCREMENTAL,
                 self.REPLICATION_KEYS: {"updatedAt"},
+                self.OBEYS_START_DATE: True
             },
             "owners": {
                 self.PRIMARY_KEYS: {"ownerId"},  # DOCS_BUG docs list 'portalId'
                 self.REPLICATION_METHOD: self.INCREMENTAL,
                 self.REPLICATION_KEYS: {"updatedAt"},
+                self.OBEYS_START_DATE: True  # TODO is this a BUG?
             },
             "subscription_changes": {
                 self.PRIMARY_KEYS: {"timestamp", "portalId", "recipient"},
                 self.REPLICATION_METHOD: self.INCREMENTAL,
                 self.REPLICATION_KEYS: {"startTimestamp"},  # DOCS_BUG docs list 'timestamp'
-                self.EXPECTED_PAGE_SIZE: 1000
+                self.EXPECTED_PAGE_SIZE: 1000,
+                self.OBEYS_START_DATE: True
             },
             "workflows": {
                 self.PRIMARY_KEYS: {"id"},
                 self.REPLICATION_METHOD: self.INCREMENTAL,
                 self.REPLICATION_KEYS: {"updatedAt"},
+                self.OBEYS_START_DATE: True
             }
         }
 
