eb55f0b18b177c086e9dcf533a7292803d1e89ab
Andreas Hansson <andreas.hansson@dreamdata.io>
2022-04-08 15:19:02 +0200

    Add legacy function of how we get companies

--- tap_hubspot/hubspot.py
+++ tap_hubspot/hubspot.py
@@ -15,7 +15,6 @@ class RetryAfterReauth(Exception):
 LOGGER = singer.get_logger()
 DATE_FORMAT = "%Y-%m-%dT%H:%M:%S.%fZ"
 
-
 def chunker(iter: Iterable[Dict], size: int) -> Iterable[List[Dict]]:
     i = 0
     chunk = []
@@ -321,6 +320,21 @@ class Hubspot:
     def get_archived_deals(self):
         object_type="deals"
         yield from self.get_archived(object_type=object_type)
+
+    def get_companies_legacy(self):
+        path = "/crm/v3/objects/companies"
+        data_field = "results"
+        properties = self.get_object_properties("companies")
+        replication_path = ["updatedAt"]
+        params = {"limit": 100, "properties": properties}
+        offset_key = "after"
+        yield from self.get_records(
+            path,
+            replication_path,
+            params=params,
+            data_field=data_field,
+            offset_key=offset_key,
+        )
 
     def get_companies(
         self, start_date: datetime, end_date: datetime
