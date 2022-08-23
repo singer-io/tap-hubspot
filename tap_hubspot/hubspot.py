904e633ff4f177aeaefa54942b3ab6b77ae25b23
iker-camara <iker.camara@dreamdata.io>
2022-08-23 14:07:46 +0200

    new fields cloudtalk_io

--- tap_hubspot/hubspot.py
+++ tap_hubspot/hubspot.py
@@ -156,8 +156,8 @@ MANDATORY_PROPERTIES = {
         "lead_source",  # cloudtalk_io
         "approved_trial",  # cloudtalk_io
         "mrr",  # cloudtalk_io,
-        "konzervativna_hodnota", # cloudtalk_io
-        "users_range_conservative", # cloudtalk_io
+        "new_seat_range_strategy_calculation", # cloudtalk_io
+        "calculated_users_range_conservative", # cloudtalk_io
         "closedate",  # getmagic_com
         "lifecyclestage",  # getmagic_com
         "contact_type",  # getmagic_com
