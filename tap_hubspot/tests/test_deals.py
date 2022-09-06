fdc420144d046cf789ce799d4a3f746b560f16d6
kethan1122 <kcherukuri@talend.com>
2022-09-06 16:44:04 +0530

    changing methods to functions since there is no-self-use

--- tap_hubspot/tests/test_deals.py
+++ tap_hubspot/tests/test_deals.py
@@ -1,31 +1,24 @@
 from tap_hubspot import sync_deals
-import unittest
 from unittest.mock import patch, ANY
 
 
-class TestDealsToSync(unittest.TestCase):
+@patch('tap_hubspot.Context.get_catalog_from_id', return_value={"metadata": ""})
+@patch('tap_hubspot.gen_request', return_value=[])
+def test_associations_are_not_validated(mocked_gen_request, mocked_catalog_from_id):
 
-    @patch('tap_hubspot.Context.get_catalog_from_id', return_value={"metadata":""})
-    @patch('tap_hubspot.gen_request', return_value=[])
-    def test_associations_are_not_validated(self,
-        mocked_gen_request,
-        mocked_catalog_from_id):
+    sync_deals({}, mocked_catalog_from_id)
 
-        sync_deals({}, mocked_catalog_from_id)
+    expected_param = {'includeAssociations': False, 'properties': [], 'limit': 100}
 
-        expected_param = {'includeAssociations': False, 'properties': [], 'limit': 100}
-
-        mocked_gen_request.assert_called_once_with(ANY, ANY, ANY, expected_param, ANY, ANY, ANY, ANY, v3_fields=None)
+    mocked_gen_request.assert_called_once_with(ANY, ANY, ANY, expected_param, ANY, ANY, ANY, ANY, v3_fields=None)
 
 
-    @patch('tap_hubspot.Context.get_catalog_from_id', return_value={"metadata":""})
-    @patch('tap_hubspot.gen_request', return_value=[])
-    def test_associations_are_validated(self,
-        mocked_gen_request,
-        mocked_catalog_from_id):
+@patch('tap_hubspot.Context.get_catalog_from_id', return_value={"metadata": ""})
+@patch('tap_hubspot.gen_request', return_value=[])
+def test_associations_are_validated(mocked_gen_request, mocked_catalog_from_id):
 
-        sync_deals({}, mocked_catalog_from_id)
+    sync_deals({}, mocked_catalog_from_id)
 
-        expected_param = {'includeAssociations': True, 'properties': [], 'limit': 100}
+    expected_param = {'includeAssociations': True, 'properties': [], 'limit': 100}
 
-        mocked_gen_request.assert_called_once_with(ANY, ANY, ANY, expected_param, ANY, ANY, ANY, ANY, v3_fields=None)
+    mocked_gen_request.assert_called_once_with(ANY, ANY, ANY, expected_param, ANY, ANY, ANY, ANY, v3_fields=None)
