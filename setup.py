a26eb449d50a9a8b4ba171961870daf81146e0ec
Josué M. Sehnem <josue@sehnem.com>
2023-02-16 13:28:28 -0500

    updated recently added to lists

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='tap-hubspot',
-      version='2.9.7',
+      version='2.9.8',
       description='Singer.io tap for extracting data from the HubSpot API',
       author='Stitch',
       url='http://singer.io',
