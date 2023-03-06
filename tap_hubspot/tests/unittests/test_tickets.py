9a3197b042939fb0263ae24199966541e6e85557
Sourabh Gandhi <sgandhi@talend.com>
2023-03-06 18:08:32 +0000

    Modified unit test case for the custom properties scenarios

--- tap_hubspot/tests/unittests/test_tickets.py
+++ tap_hubspot/tests/unittests/test_tickets.py
@@ -1,117 +1,67 @@
 from unittest.mock import patch, ANY
 from tap_hubspot import sync_tickets, gen_request_tickets
-from unittest.mock import patch
+import unittest
 
+mock_response_data = {
+        "results": [{
+		"updatedAt": "2022-08-18T12:57:17.587Z",
+		"createdAt": "2019-08-06T02:43:01.930Z",
+		"name": "hs_file_upload",
+		"label": "File upload",
+		"type": "string",
+		"fieldType": "file",
+		"description": "Files attached to a support form by a contact.",
+		"groupName": "ticketinformation",
+		"options": [],
+		"displayOrder": -1,
+		"calculated": False,
+		"externalOptions": False,
+		"hasUniqueValue": False,
+		"hidden": False,
+		"hubspotDefined": True,
+		"modificationMetadata": {
+			"archivable": True,
+			"readOnlyDefinition": True,
+			"readOnlyValue": False
+		},
+		"formField": True
+	}]
+}
+    
 
 class MockResponse:
 
-    def __init__(self, json_data, status_code):
+    def __init__(self, json_data):
         self.json_data = json_data
-        self.status_code = status_code
 
     def json(self):
         return self.json_data
 
-
-def get_mocked_response(mock_output):
-    return MockResponse(mock_output, status_code=200)
-
-
-@patch('tap_hubspot.Context.get_catalog_from_id',
-       return_value={'metadata': ''})
-@patch('tap_hubspot.singer.utils.strptime_with_tz',
-       return_value='2017-01-01 00:00:00+00:00')
-@patch('tap_hubspot.singer.utils.strftime')
-@patch('tap_hubspot.get_start', return_value='')
-@patch('tap_hubspot.gen_request_tickets', return_value=[])
-def test_ticket_params_are_validated(mocked_gen_request, mocked_catalog_from_id,
-                                     mocked_get_start, mocked_utils_strptime,
-                                     mocked_utils_strftime,):
-    """
-    # Validating the parameters passed while making the API request to list the tickets
-    """
-
-    sync_tickets({}, mocked_catalog_from_id)
-
-    expected_param = {'limit': 100,
-                      'associations': 'contact,company,deals',
-                      'archived': False
-                      }
-    mocked_gen_request.assert_called_once_with(ANY, ANY, expected_param, ANY, ANY,)
-
-output_results = {
-    'results': [{
-        'id': '190756785',
-        'properties': {
-            'content': 'also a test',
-            'createdate': '2020-08-26T15:18:14.135Z',
-            'hs_lastmodifieddate': '2020-11-24T19:45:08.380Z',
-            'hs_object_id': '190756785',
-            'hs_pipeline': '0',
-            'hs_pipeline_stage': '1',
-            'hs_ticket_category': None,
-            'hs_ticket_priority': 'MEDIUM',
-            'subject': 'ticket 2',
-        },
-        'createdAt': '2020-08-26T15:18:14.135Z',
-        'updatedAt': '2020-11-24T19:45:08.380Z',
-        'archived': False,
-        'associations': {
-            'companies': {
-                'results': [{
-                    'id': '635376281',
-                    'type': 'ticket_to_company'
-                }, {
-                    'id': '635376281',
-                    'type': 'ticket_to_company_unlabeled'
-                }]
-            },
-            'deals': {
-                'results': [{
-                    'id': '2870386661',
-                    'type': 'ticket_to_deal'
-                }]
-            },
-            'contacts': {
-                'results': [{
-                    'id': '1',
-                    'type': 'ticket_to_contact'
-                }, {
-                    'id': '51',
-                    'type': 'ticket_to_contact'
-                }, {
-                    'id': '101',
-                    'type': 'ticket_to_contact'
-                }]
-            }
-        },
-    }]
-}
+class MockContext:
+    def get_catalog_from_id(self, stream_name):
+        return {"stream":"tickets","tap_stream_id":"tickets","schema":{"type":"object","properties":{"id":{"type":"string"},"createdAt":{"type":["null","string"],"format":"date-time"},"updatedAt":{"type":["null","string"],"format":"date-time"},"properties":{"type":"object","properties":{"closed_date":{"type":["null","string"],"format":"date-time"},"hs_all_team_ids":{"type":["null","string"]}}},"associations":{"type":["null","object"],"properties":{"companies":{"type":["null","object"],"properties":{"results":{"type":["null","array"],"items":{"type":["null","object"],"properties":{"id":{"type":["null","string"]},"type":{"type":["null","string"]}}}}}}}},"property_closed_date":{"type":["null","string"],"format":"date-time"},"property_hs_all_team_ids":{"type":["null","string"]}}},"metadata":[{"breadcrumb":[],"metadata":{"table-key-properties":["id"],"forced-replication-method":"INCREMENTAL","valid-replication-keys":["updatedAt"],"selected":True}},{"breadcrumb":["properties","id"],"metadata":{"inclusion":"automatic"}},{"breadcrumb":["properties","createdAt"],"metadata":{"inclusion":"available"}},{"breadcrumb":["properties","updatedAt"],"metadata":{"inclusion":"automatic"}},{"breadcrumb":["properties","properties"],"metadata":{"inclusion":"available"}},{"breadcrumb":["properties","associations"],"metadata":{"inclusion":"available"}},{"breadcrumb":["properties","property_closed_date"],"metadata":{"inclusion":"available", "selected": True}},{"breadcrumb":["properties","property_hs_all_team_ids"],"metadata":{"inclusion":"available"}}]}
 
 
-@patch('tap_hubspot.Context.get_catalog_from_id',
-       return_value={'metadata': ''})
-@patch('tap_hubspot.singer.utils.strptime_with_tz',
-       return_value='2017-01-01 00:00:00+00:00')
-@patch('tap_hubspot.singer.utils.strftime')
-@patch('tap_hubspot.get_start', return_value='')
-@patch('tap_hubspot.request')
-def test_ticket_gen_request_validated(mocked_request, mocked_catalog_from_id,
-                                      mocked_get_start, mocked_utils_strptime,
-                                      mocked_utils_strftime,):
-    """
-    # Checking if gen_request_tickets() is calling request function atleast once
-    """
+class TestTickets(unittest.TestCase):
 
-    mocked_request.return_value = get_mocked_response(output_results)
-    sync_tickets({}, mocked_catalog_from_id)
-    input_params = {'limit': 100,
-                    'associations': 'contact,company,deals',
-                    'archived': False
-                    }
-    input_url = 'https://api.hubapi.com/crm/v4/objects/tickets'
-    stream_id = 'tickets'
-    gen_request_tickets(stream_id, input_url, input_params,
-                        'results', 'paging',)
+    @patch('tap_hubspot.request', return_value=MockResponse(mock_response_data))
+    @patch('tap_hubspot.get_start', return_value='2023-01-01T00:00:00Z')
+    @patch('tap_hubspot.gen_request_tickets')
+    def test_ticket_params_are_validated(self, mocked_gen_request, mocked_get_start, mock_request_response):
+        """
+        # Validating the parameters passed while making the API request to list the tickets
+        """
+        mock_context = MockContext()
+        expected_param = {'limit': 100,
+                        'associations': 'contact,company,deals',
+                        'archived': False
+                        }
+        expected_return_value = {'currently_syncing': 'tickets', 'bookmarks': {'tickets': {'updatedAt': '2023-01-01T00:00:00.000000Z'}}}
+        
+        return_value = sync_tickets({'currently_syncing': 'tickets'}, mock_context)
+        self.assertEqual(
+                        expected_return_value,
+                        return_value
+                    )
+        mocked_gen_request.assert_called_once_with('tickets', 'https://api.hubapi.com/crm/v4/objects/tickets', {'limit': 100, 'associations': 'contact,company,deals', 'properties': 'closed_date', 'archived': False}, 'results', 'paging')
 
-    mocked_request.assert_called_once_with(input_url, input_params)
