fbded5686e77ea0125c2129d7517ecbbe1ee0e08
RushiT0122 <rtodkar@stitchdata-talend.com>
2023-04-25 11:24:13 +0000

    update unit test

--- tap_hubspot/tests/test_deals.py
+++ tap_hubspot/tests/test_deals.py
@@ -1,14 +1,19 @@
 from tap_hubspot import sync_deals
 from unittest.mock import patch, ANY
 
+# from debugpy import listen, wait_for_client
+# listen(8000)
+# wait_for_client()
 
+
+@patch('builtins.min')
 @patch('tap_hubspot.Context.get_catalog_from_id', return_value={"metadata": ""})
 @patch('singer.metadata.to_map', return_value={})
 @patch('singer.utils.strptime_with_tz')
 @patch('singer.utils.strftime')
 @patch('tap_hubspot.load_schema')
 @patch('tap_hubspot.gen_request', return_value=[])
-def test_associations_are_not_validated(mocked_gen_request, mocked_catalog_from_id, mocked_metadata_map, mocked_utils_strptime, mocked_utils_strftime, mocked_load_schema):
+def test_associations_are_not_validated(mocked_gen_request, mocked_catalog_from_id, mocked_metadata_map, mocked_utils_strptime, mocked_utils_strftime, mocked_load_schema, mocked_min):
     # pylint: disable=unused-argument
     sync_deals({}, mocked_catalog_from_id)
 
@@ -17,13 +22,14 @@ def test_associations_are_not_validated(mocked_gen_request, mocked_catalog_from_
     mocked_gen_request.assert_called_once_with(ANY, ANY, ANY, expected_param, ANY, ANY, ANY, ANY, v3_fields=None)
 
 
+@patch('builtins.min')
 @patch('tap_hubspot.Context.get_catalog_from_id', return_value={"metadata": ""})
 @patch('singer.metadata.to_map', return_value={"associations": {"selected": True}})
 @patch('singer.utils.strptime_with_tz')
 @patch('singer.utils.strftime')
 @patch('tap_hubspot.load_schema')
 @patch('tap_hubspot.gen_request', return_value=[])
-def test_associations_are_validated(mocked_gen_request, mocked_catalog_from_id, mocked_metadata_map, mocked_utils_strptime, mocked_utils_strftime, mocked_load_schema):
+def test_associations_are_validated(mocked_gen_request, mocked_catalog_from_id, mocked_metadata_map, mocked_utils_strptime, mocked_utils_strftime, mocked_load_schema, mocked_min):
     # pylint: disable=unused-argument
     sync_deals({}, mocked_catalog_from_id)
 
