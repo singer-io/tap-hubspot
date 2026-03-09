c922a1378515a000a7af8b1e809f4362a6e5174b
Ben Allred <ben.allred@qlik.com>
2026-03-09 11:47:23 -0600

    SAC-30347: fix interrupted sync test (#293)
    * uncomment assertions
    
    * create ticket/engagement in test
    
    * refactor
    
    * ensure ticket/enagement before simulating interruption
    
    * fix typo
    
    * fix typo 2
    
    * ensure engagement and ticket before first sync
    
    * fix missing param

--- tests/test_hubspot_interrupted_sync.py
+++ tests/test_hubspot_interrupted_sync.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from time import sleep
 import copy
+import requests
 
 import tap_tester.connections as connections
 import tap_tester.menagerie   as menagerie
@@ -18,9 +19,52 @@ class TestHubspotInterruptedSync1(HubspotBaseTest):
 
     def streams_to_test(self):
         """expected streams minus the streams not under test"""
-        # BUG https://qlik-dev.atlassian.net/browse/SAC-30347,
-        # add tickets and engagements into streams_to_test after resolving
-        return {'companies', 'contacts'}
+        return {'companies', 'contacts', 'tickets', 'engagements'}
+
+    def get_access_token(self):
+        payload = {
+            'grant_type': 'refresh_token'
+        }
+        payload.update(self.get_credentials())
+
+        resp = requests.post("https://api.hubapi.com/oauth/v1/token", data=payload)
+        return resp.json()['access_token']
+
+    def ensure_ticket(self, headers):
+        ticket = [
+            {
+                'name': 'subject',
+                'value': 'Example ticket subject'
+            },
+            {
+                'name': 'hs_pipeline',
+                'value': '0'
+            },
+            {
+                'name': 'hs_pipeline_stage',
+                'value': '1'
+            }
+        ]
+        requests.post('https://api.hubapi.com/crm-objects/v1/objects/tickets', json=ticket, headers=headers)
+
+    def ensure_engagement(self, headers):
+        engagement = {
+            'engagement': {
+                'type': 'EMAIL',
+                'active': True
+            },
+            'metadata': {}
+        }
+        requests.post('https://api.hubapi.com/engagements/v1/engagements', json=engagement, headers=headers)
+
+    def ensure_engagement_and_ticket(self):
+        access_token = self.get_access_token()
+        headers = {
+            'authorization': f'Bearer {access_token}',
+            'content-type': 'application/json'
+        }
+        self.ensure_engagement(headers)
+        self.ensure_ticket(headers)
 
     def simulated_interruption(self, reference_state):
 
@@ -33,18 +77,17 @@ class TestHubspotInterruptedSync1(HubspotBaseTest):
         new_state['bookmarks']['companies']['property_hs_lastmodifieddate'] = None
         new_state['bookmarks']['companies']['current_sync_start'] = companies_bookmark
 
-        # Uncomment after SAC-30347 is resloved
-        # engagements_bookmark = self.timedelta_formatted(
-        #     reference_state['bookmarks']['engagements']['lastUpdated'],
-        #     days=-1, str_format=self.BASIC_DATE_FORMAT
-        # )
-        # new_state['bookmarks']['engagements']['lastUpdated'] = None
-        # new_state['bookmarks']['engagements']['current_sync_start'] = engagements_bookmark
+        engagements_bookmark = self.timedelta_formatted(
+            reference_state['bookmarks']['engagements']['lastUpdated'],
+            days=-1, str_format=self.BASIC_DATE_FORMAT
+        )
+        new_state['bookmarks']['engagements']['lastUpdated'] = None
+        new_state['bookmarks']['engagements']['current_sync_start'] = engagements_bookmark
 
-        # tickets_bookmark = self.timedelta_formatted(
-        #     reference_state['bookmarks']['tickets']['updatedAt'],
-        #     days=-1, str_format=self.BASIC_DATE_FORMAT)
-        # new_state['bookmarks']['tickets']['updatedAt'] = tickets_bookmark
+        tickets_bookmark = self.timedelta_formatted(
+            reference_state['bookmarks']['tickets']['updatedAt'],
+            days=-1, str_format=self.BASIC_DATE_FORMAT)
+        new_state['bookmarks']['tickets']['updatedAt'] = tickets_bookmark
 
         contacts_bookmark = self.timedelta_formatted(
             reference_state['bookmarks']['contacts']['updatedAt'],
@@ -54,8 +97,6 @@ class TestHubspotInterruptedSync1(HubspotBaseTest):
         return new_state
 
     def get_properties(self):
-        #        'start_date' : '2021-08-19T00:00:00Z'
-        # return {'start_date' : '2017-11-22T00:00:00Z'}
         return {
             'start_date' : datetime.strftime(
                 datetime.today()-timedelta(days=5), self.START_DATE_FORMAT
@@ -84,6 +125,7 @@ class TestHubspotInterruptedSync1(HubspotBaseTest):
             )
 
         # Run sync 1
+        self.ensure_engagement_and_ticket()
         first_record_count_by_stream = self.run_and_verify_sync(conn_id)
         synced_records = runner.get_records_from_target_output()
         state_1 = menagerie.get_state(conn_id)
